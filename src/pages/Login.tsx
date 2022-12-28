4d759e56f4cc00add80b4401bc3312461e9f8ae0
kinoshita0923 <sota0923ks15@gmail.com>
2022-12-28 22:52:55 +0900

    ♻️ いらないインポートの削除、コード整形を行った

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
 import FilledInput from '@mui/material/FilledInput'
 import classes from '../styles/Login.module.css'
 
@@ -23,14 +21,16 @@ const inputStyle = {
 
 const Login = () => {
     return (
-        <div className={classes.page}>
-            <div className={classes.inputs_box}>
-                <img src="./manabiba.png" className={classes.logo} />
-                <h1 className={classes.title}>manabiba Login</h1>
-                <FilledInput placeholder='email' sx={inputStyle} required />
-                <FilledInput placeholder='password' sx={inputStyle} required />
-                <Link to="/register" className={classes.register}>新規登録</Link>
-                <Button variant="contained" sx={buttonStyle} >LogIn</Button>
+        <div className={classes.background}>
+            <div className={classes.page}>
+                <div className={classes.inputs_box}>
+                    <img src="./manabiba.png" className={classes.logo} />
+                    <h1 className={classes.title}>manabiba Login</h1>
+                    <FilledInput placeholder='email' sx={inputStyle} required />
+                    <FilledInput placeholder='password' sx={inputStyle} required />
+                    <Link to="/register" className={classes.register}>新規登録</Link>
+                    <Button variant="contained" sx={buttonStyle} >LogIn</Button>
+                </div>
             </div>
         </div>
     )
